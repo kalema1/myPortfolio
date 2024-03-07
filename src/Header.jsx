538edbd5f7ac010b069e7d73856bab8d0047cfc2
kalema1 <kalemar8@gmail.com>
2024-03-07 08:40:50 +0300

    feat(mobile menu): add featue to display menu on mobile device
    - added logic to display navigation list on mobie devices.
    - added style for navigation list on mobile devices.
    -added a menu button to be used to display navigation list.

--- src/Header.jsx
+++ src/Header.jsx
@@ -1,6 +1,15 @@
+import { useState } from "react";
+import { IoIosMenu, IoIosClose } from "react-icons/io";
+
 export default function Header() {
+  const [showNav, setShowNav] = useState(false);
+
+  function handleShowNavigation() {
+    setShowNav((navBar) => !navBar);
+  }
+
   return (
-    <header className="header">
+    <header className={`header ${showNav ? "show-nav" : ""}`}>
       <div className="container position-fixed">
         <nav className="nav-bar">
           <div className="logo">kalema</div>
@@ -20,7 +29,29 @@ export default function Header() {
               </li>
             </ul>
           </div>
-          <div className="toggle"></div>
+          <div className="toggle" onClick={handleShowNavigation}>
+            {showNav ? (
+              <IoIosClose className="menu-icon" />
+            ) : (
+              <IoIosMenu className="menu-icon" />
+            )}
+          </div>
+          <div className="mobile-nav-list">
+            <ul className="mobile-nav" onClick={handleShowNavigation}>
+              <li className="nav-list-item">
+                <a href="#home">Home</a>
+              </li>
+              <li className="nav-list-item">
+                <a href="#about">About</a>
+              </li>
+              <li className="nav-list-item">
+                <a href="#projects">Projects</a>
+              </li>
+              <li className="nav-list-item">
+                <a href="#contact">Contant</a>
+              </li>
+            </ul>
+          </div>
         </nav>
       </div>
     </header>
--- src/data/data.js
+++ src/data/data.js
@@ -3,7 +3,7 @@ export const projectDataList = [
     id: 1,
     title: "Data web Application",
     description:
-      "This is a Frontend of the Data web application. It monitors data to increase revenue for diefferent Platforms. For a live Demo",
+      "This is a Frontend of the Data web application. It monitors data to increase revenue for different Platforms. For a live Demo",
     image: "datafinace.PNG",
     projectDemoLink: "https://the-react-data.netlify.app/",
   },
@@ -19,7 +19,7 @@ export const projectDataList = [
     id: 3,
     title: "Dav Safaris Application",
     description:
-      "This is a Tour and Travel Agency appplication for Dav Safaris based in Uganda. Offers a wide variety of Tour services across East Africa. For a live Demo",
+      "This is a Tour and Travel Agency application for Dav Safaris based in Uganda. Offers a wide variety of Tour services across East Africa. For a live Demo",
     image: "dav.PNG",
     projectDemoLink: "https://dev-safaris.web.app/",
   },
