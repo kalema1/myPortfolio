0d04ca7ed7ce9e0b790f8334a23c62aa69ab072d
kalema1 <kalemar8@gmail.com>
2024-07-03 11:34:10 +0300

    Update projectDataList
    - added a list item to the projectDataList items.
    - added a new image for the new list item.

--- src/data/data.js
+++ src/data/data.js
@@ -1,6 +1,14 @@
 export const projectDataList = [
   {
     id: 1,
+    title: "Property Pulse",
+    description:
+      "This is a web application to help you find your next Rental Property.User can search for a property, logged in user can add a property for rent, share a property, List properties available for rent. The user can contact the owner of the property. This application has a shere feature, to allow users share properties on social media.",
+    image: "properties.PNG",
+    projectDemoLink: "https://dev-safaris.web.app/",
+  },
+  {
+    id: 2,
     title: "Data web Application",
     description:
       "This is a Frontend of the Data web application. It monitors data to increase revenue for different Platforms. For a live Demo",
@@ -8,7 +16,7 @@ export const projectDataList = [
     projectDemoLink: "https://the-react-data.netlify.app/",
   },
   {
-    id: 2,
+    id: 3,
     title: "GPA Elevator Application",
     description:
       "This application enables the  university students to improve their GPA(Grading Points Average). It enables users access all the reliable resources with no limit. For a live Demo",
@@ -16,7 +24,7 @@ export const projectDataList = [
     projectDemoLink: "https://gpa-elavator.firebaseapp.com/",
   },
   {
-    id: 3,
+    id: 4,
     title: "Dav Safaris Application",
     description:
       "This is a Tour and Travel Agency application for Dav Safaris based in Uganda. Offers a wide variety of Tour services across East Africa. For a live Demo",
